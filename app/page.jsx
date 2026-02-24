92c89bc0b39d1061e0261a29fa703a5550364048
Naman Gupta <naman.gupta@pm.me>
2026-02-24 23:23:24 +0530

    to jekyll

--- app/page.jsx
+++ app/page.jsx
@@ -10,9 +10,9 @@ import {
 
 /** @type {import('next').Metadata} */
 export const metadata = {
-  title: 'Home | Dennis Snellenberg',
+  title: 'Namita Gupta',
   description:
-    'Helping brands thrive in the digital world. Located in The Netherlands. Delivering tailor-made digital designs and building interactive websites from scratch. © Code by Dennis',
+    '',
 };
 
 export default function Home() {
